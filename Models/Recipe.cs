49c35b7d7a83dad5b566eebc4f4f772405fcaa07
Ahmed-20-02 <c0019714@my.shu.ac.uk>
2021-03-08 19:40:05 +0000

    ViewAll for Admin. User, Recipe Classes.

---
+++ Models/Recipe.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipesGalorePRJ.Models
+{
+    public class Recipe
+    {
+        
+        public int RecipeId { get; set; }
+
+        [Display(Name = "Recipe Name")]
+        public string RecipeName { get; set; }
+
+        [Display(Name = "Recipe Ingredients")]
+        public string RecipeCuisineType { get; set; }
+
+        [Display(Name = "Recipe Cook Time")]
+        public int RecipeCookingTime { get; set; }
+
+        [Display(Name = "Recipe Ingredients")]
+        public string RecipeIngredients { get; set; }
+
+        [Display(Name = "Recipe Method")]
+        public string RecipeMethod { get; set; }
+
+        public string File { get; set; }
+
+        public User Creator { get; set; }
+    }
+}
