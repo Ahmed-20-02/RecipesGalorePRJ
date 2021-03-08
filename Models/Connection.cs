370d3239f1db12a2079cde92c7c221e1691efb3e
Ahmed-20-02 <c0019714@my.shu.ac.uk>
2021-03-08 21:26:29 +0000

    Added Connection String Class

---
+++ Models/Connection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipesGalorePRJ.Models
+{
+    public class Connection
+    {
+        private string ConnectionString = "";
+
+        public string ReturnConnectionString()
+        {
+            return ConnectionString;
+        }
+    }
+}
